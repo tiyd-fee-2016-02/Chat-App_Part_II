a313a7e5edfd91081f5b10d23218554e7f42efa3
Tori Hedden <tori.hedden@gmail.com>
2016-03-08 18:13:05 -0500

    adds simple temp function, for now only for NC

---
+++ assets/js/chat_functions/atTemp.js
@@ -0,0 +1,12 @@
+var atTemp = function(){
+
+  $.getJSON("http://api.wunderground.com/api/b80f8aa82340bfd9/conditions/q/" + "NC" + "/" + APIValue + ".json", function(json) {
+
+    var tempRound = Math.round(json.current_observation.temp_f);
+
+    $(".chat-container").append("<div class = 'alert alert-info' role = 'alert'>" + tempRound + "</div>")
+
+  });
+
+
+};
