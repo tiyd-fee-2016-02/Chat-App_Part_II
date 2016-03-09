2acb1cd424fefa1dcc0338622065855a1f79ad4a
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-09 09:11:18 -0500

    bringing definition back to json call

--- assets/js/chat_functions/atDefinition.js
+++ assets/js/chat_functions/atDefinition.js
@@ -7,23 +7,23 @@
 
     $(".chat-container").append("<div class = 'alert alert-success text-right' role = 'alert'>" + $(".chat-input").val() + "</div>")
 
-    // $.getJSON("http://dictionaryapi.net/api/definition/" + firstValue , function(json){
-    //      console.log("got json", json);
-    //
-    //      var jsonstring = JSON.stringify(json[0].definitions)
+    $.getJSON("http://dictionaryapi.net/api/definition/" + firstValue , function(json){
+         console.log("got json", json);
 
-    $.ajax({
-      url: 'http://dictionaryapi.net/api/definition/' + firstValue,
-      type: 'GET',
-      dataType: 'jsonp'
-    }).done(function (json) {
-      console.log("got json", json);
+        //  var jsonstring = JSON.stringify(json[0].definitions)
+
+    // $.ajax({
+    //   url: 'http://dictionaryapi.net/api/definition/' + firstValue,
+    //   type: 'GET',
+    //   dataType: 'jsonp'
+    // }).done(function (json) {
+    //   console.log("got json", json);
 
        $(".chat-container").append("<div class = 'alert alert-info text-left' role = 'alert'>" + json[0].definitions + "</div>")
 
-});
+// });
 
-// }); closes getjson function
+}); //closes getjson function
 
 }; //closes function atDictionary
 
