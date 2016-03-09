b35a8dbc00b56e67c9e5eacab165a263df6f03b3
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-09 07:34:09 -0500

    last ditch effort for dictionary

---
+++ assets/js/chat_functions/atDefinition.js
@@ -0,0 +1,33 @@
+// $(function(){
+//   'use strict';
+
+
+
+  function atDefinition(){
+
+    $(".chat-container").append("<div class = 'alert alert-success text-right' role = 'alert'>" + $(".chat-input").val() + "</div>")
+
+    // $.getJSON("http://dictionaryapi.net/api/definition/" + firstValue , function(json){
+    //      console.log("got json", json);
+    //
+    //      var jsonstring = JSON.stringify(json[0].definitions)
+
+    $.ajax({
+      url: 'http://dictionaryapi.net/api/definition/' + firstValue,
+      type: 'GET',
+      dataType: 'jsonp'
+    }).done(function (json) {
+      console.log("got json", json);
+
+       $(".chat-container").append("<div class = 'alert alert-info text-left' role = 'alert'>" + json[0].definitions + "</div>")
+
+});
+
+// }); closes getjson function
+
+}; //closes function atDictionary
+
+// var dict = $.getJSON("http://dictionaryapi.net/api/definition/" + text)
+//             dict.done(function(data) {
+//               console.log("got data", data);
+//             });
